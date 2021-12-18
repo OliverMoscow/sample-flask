5cc0ec5fa04bcb9334dd20c14b5307dc823f8535
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2021-12-18 16:39:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ from sendgrid.helpers.mail import Mail
 # from email.mime.multipart import MIMEMultipart
 # from email.mime.text import MIMEText
 
+# $ export FLASK_APP=hello
+# $ flask run
+#  * Running on http://127.0.0.1:5000/
+
 load_dotenv()
 
 
